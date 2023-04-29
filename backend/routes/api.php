3acee0e8df19d1eec943cc5feffd8e1fc44570aa
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-04-30 00:30:24 +0600

    Changed auth and api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'
+], function ($router) {
+    Route::post('register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+    Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::post('refresh', [\App\Http\Controllers\Api\AuthController::class, 'refresh']);
+    Route::post('me', [\App\Http\Controllers\Api\AuthController::class, 'getUsers']);
+
+});
